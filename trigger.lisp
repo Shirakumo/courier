9ae3cc484a984c2335389aeec608ef52d7f07ccc
Shinmera <shinmera@tymoon.eu>
2020-04-16 13:43:25 +0200

    Add help for tag constraints and expand them to allow ORs.

--- trigger.lisp
+++ trigger.lisp
@@ -10,18 +10,24 @@
   (with-input-from-string (in constraint)
     (let ((buffer (make-string-output-stream))
           (mode :include)
+          (groups ())
           (tokens ()))
-      (flet ((end-token ()
-               (let ((token (get-output-stream-string buffer)))
-                 (when (string/= "" token)
-                   (push (cons mode token) tokens))
-                 (setf mode :include))))
+      (labels ((end-token ()
+                 (let ((token (get-output-stream-string buffer)))
+                   (when (string/= "" token)
+                     (push (cons mode token) tokens))
+                   (setf mode :include)))
+               (end-group ()
+                 (end-token)
+                 (when tokens (push (nreverse tokens) groups))
+                 (setf tokens ())))
         (loop for char = (read-char in NIL)
               while char
               do (case char
                    (#\+ (end-token))
                    (#\- (end-token) (setf mode :exclude))
                    (#\  (end-token))
+                   (#\| (end-group))
                    (#\\ (write-char (read-char in) buffer))
                    (#\" (loop for char = (read-char in NIL)
                               do (case char
@@ -31,39 +37,43 @@
                                    (T (write-char char buffer))))
                     (end-token))
                    (T (write-char char buffer)))
-              finally (end-token))
-        (nreverse tokens)))))
+              finally (end-group))
+        (nreverse groups)))))
 
 (defun normalize-constraint (campaign constraint)
   (with-output-to-string (out)
-    (loop for (mode . tag-name) in (parse-constraint constraint)
-          for tag = (dm:get-one 'tag (db:query (:and (:= 'campaign (ensure-id campaign))
-                                                     (:= 'title tag-name))))
-          do (cond (tag
-                    (write-char (ecase mode
-                                  (:include #\+)
-                                  (:exclude #\-))
-                                out)
-                    (princ (dm:id tag) out))
-                   (T
-                    (cerror "Ignore the tag" "No such tag ~s" tag-name))))))
+    (loop for group in (parse-constraint constraint)
+          do (loop for (mode . tag-name) in group
+                   for tag = (dm:get-one 'tag (db:query (:and (:= 'campaign (ensure-id campaign))
+                                                              (:= 'title tag-name))))
+                   do (cond (tag
+                             (write-char (ecase mode
+                                           (:include #\+)
+                                           (:exclude #\-))
+                                         out)
+                             (princ (dm:id tag) out))
+                            (T
+                             (cerror "Ignore the tag" "No such tag ~s" tag-name))))
+             (write-char #\| out))))
 
 (defun tag-constraint-applicable-p (normalized-constraint subscriber)
   (let ((tags (db:iterate 'tag-table (db:query (:= 'subscriber (ensure-id subscriber)))
                           (lambda (r) (gethash "tag" r)) :fields '("tag") :accumulate T)))
     (loop with start = 0
           with mode = :inclusive
+          with passed = T
           for i from 0 below (length normalized-constraint)
           do (flet ((check-tag ()
                       (let ((id (db:ensure-id (subseq normalized-constraint start i))))
                         (setf start (1+ i))
                         (if (find id tags :test #'equal)
-                            (when (eq :exclude mode) (return NIL))
-                            (when (eq :include mode) (return NIL))))))
+                            (when (eq :exclude mode) (setf passed NIL))
+                            (when (eq :include mode) (setf passed NIL))))))
                (case (char normalized-constraint i)
                  (#\+ (check-tag) (setf mode :include))
-                 (#\- (check-tag) (setf mode :exclude))))
-          finally (return T))))
+                 (#\- (check-tag) (setf mode :exclude))
+                 (#\| (when passed (return T)))))
+          finally (return passed))))
 
 (defun process-trigger (subscriber trigger &key force)
   (when (or force
